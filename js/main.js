f24a4f9815fca31be45793336c5a486ff02b5dcd
whartley <whartleybooth@live.co.uk>
2017-04-18 12:00:23 +0100

    Test

--- js/main.js
+++ js/main.js
@@ -2,14 +2,7 @@ $(function(){
 
   $('#generate_click_callback').click(function() {
 
-    var rum = {
-             page: window.location,
-             cid: "24l339123",
-             act: 1,
-         };
-     rum = reduce(rum);
-     navigator.sendBeacon('http://tracker.marinsm.com/tp?', JSON.stringify(rum));
-}, false);
+  console.log('test');
 
 
   })
