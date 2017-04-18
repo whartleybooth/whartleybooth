2ad050bf48de79cc9e2d05c9b2163771cb9e7804
whartley <whartleybooth@live.co.uk>
2017-04-18 12:49:22 +0100

    test

--- js/main.js
+++ js/main.js
@@ -4,14 +4,8 @@ $(function(){
 
   console.log('test');
    // marinClickCall();
-
-   var rum = {
-           navigation: 1,
-           act: 1
-       };
-   navigator.sendBeacon('https://tracker.marinsm.com/tp', JSON.stringify(rum));
-
-
+    reportEvent();
+  
   })
 });
 
@@ -20,3 +14,11 @@ function marinClickCall() {
   var track = new Image();
    track.src="https://tracker.marinsm.com/tp?act=1&cid=62571g0b12611868&tz=-1&ref=&page=http%3A%2F%2Fwhartleybooth.co.uk%2Fmarin_tests.html%3Fmkwid%3Ds5OV7lY0f";
 };
+
+function reportEvent(event) {
+    var data = JSON.stringify({
+      act: 1,
+      test: 2
+    });
+    navigator.sendBeacon('https://tracker.marinsm.com/tp', data);
+  }
