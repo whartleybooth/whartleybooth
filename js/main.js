494924fa6277d3e9584938e60c10bcd1b1f0374b
whartley <whartleybooth@live.co.uk>
2017-06-23 14:22:04 +0100

    test

--- js/main.js
+++ js/main.js
@@ -12,26 +12,24 @@ $(function(){
 
   $('#msm_test').click(function() {
 
-     var time = Date.now()
 
+     var track = new Image();
+     track.src="https://tracker.marinsm.com/tp?act=2&cid=2764ri918980&currency=GBP&trans=UTM:I|" +  dataLayer[0].transaction_timestamp + dataLayer[0].transactionId + "|lifeclickout|||" + dataLayer[0].transactionTotal + "|";
 
-     marinImage(time);
-     fbtag(time)
+    test(dataLayer[0].transaction_timestamp)
 
 });
+
+function test(time) {
+  fbq('track', 'Purchase', {
+value: 247.35,
+currency: 'USD',
+order_id: time //this is the only purchase / lead event parameter we require for this project. We obtain the other values from the Marin event code.
 });
-
-function fbtag(time) {
-  fbq('track', 'Purchase', {value: 247.35, currency: 'USD', order_id: time });
 };
 
-function marinImage(time) {
 
-var track = new Image();
-track.src="https://tracker.marinsm.com/tp?act=2&cid=2764ri918980&currency=GBP&trans=UTM:I|" + time + dataLayer[0].transactionId + "|lifeclickout|||" + dataLayer[0].transactionTotal + "|";
-
-};
-
+  })
 
 
 function marinSendBeacon(event) {
