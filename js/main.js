33de54598ce7ea37563b59bfcab592ccbd0084be
whartley <whartleybooth@live.co.uk>
2017-06-23 13:39:59 +0100

    test

--- js/main.js
+++ js/main.js
@@ -17,6 +17,12 @@ $(function(){
      var track = new Image();
      track.src="https://tracker.marinsm.com/tp?act=2&cid=2764ri918980&currency=GBP&trans=UTM:I|" + time + dataLayer[0].transactionId + "|lifeclickout|||" + dataLayer[0].transactionTotal + "|";
 
+     function() {fbq('track', 'Purchase', {
+ value: 247.35,
+ currency: 'USD',
+ order_id: 'ddfa2-aa0c-310c' //this is the only purchase / lead event parameter we require for this project. We obtain the other values from the Marin event code.
+ });
+ }
 
 });
 
